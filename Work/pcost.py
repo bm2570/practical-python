a233b2149bb6150deefa1fde83ee89a46a3263ca
Brad McNiven <bm2570@mun.ca>
2021-05-20 17:46:02 -0230

    Completed exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f)
+cost=0
+#headers'name,shares,price\n'
+for line in f:
+    #print(line, end='')
+   a,b,c=line.split(',')
+   cost=cost+(float(c)*float(b))
+f.close()
+print(f"Total cost: {cost}")
