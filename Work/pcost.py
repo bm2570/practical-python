c4d72208a4df62d849c238c699f3efd1e3e0c627
Brad McNiven <bm2570@mun.ca>
2021-05-22 11:14:19 -0230

    fixed Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,17 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
-def portfolio_cost(filename,i):
-    f = open('filename', 'rt')
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
     headers = next(f)
     cost=0
-#headers'name,shares,price\n'
     for line in f:
-    #print(line, end='')
-        a,b,c=line.split(',')
-        cost=cost+(float(c)*float(b)
-    #f.close()
+        row=line.split(',')
+        share=row[1]
+        sharecost=row[2]
+        cost=cost+int(share)*float(sharecost)
     return cost
 
-cost = portfolio_cost('Data/portfolio.csv', 'rt')
+cost = portfolio_cost('Data/portfolio.csv')#, 'rt')
 print(f"Total cost: {cost}")
+
