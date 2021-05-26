8e0c200ab2b62dd6950f995b6b4f2c7888824327
Brad McNiven <bm2570@mun.ca>
2021-05-26 14:11:51 -0230

    completed exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,22 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+import sys
+def read_portfolio(filename,rt):
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+            info = { 'name':row[0],'shares':int(row[1]),'price':float(row[2]) }
+            portfolio.append((info))
+
+    return portfolio
+filename=sys.argv[1]
+name = read_portfolio(filename,'rt')
+
+for i in range(len(name)):
+    print (name[i])
