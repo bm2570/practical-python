1ebe63e34219dcbb1e4406b11b67be2bb55999b1
Brad McNiven <bm2570@mun.ca>
2021-05-29 12:41:40 -0230

    Completd exercise 3.1

--- Work/report.py
+++ Work/report.py
@@ -5,23 +5,22 @@
 import csv
 import sys
 import numpy as np
-def read_portfolio(filename):
+def read_portfolio(filename1,rt):
     portfolio = []
-    with open(filename,'rt') as f:
+    with open(filename1) as f:
         rows = csv.reader(f)
         headers = next(rows)
 
         for row in rows:
-			record=dict(zip(headers,row))
-            info = { 'name':record['name'],'shares':int(record['shares']),'price':float(record['price']) }
+            info = { 'name':row[0],'shares':int(row[1]),'price':float(row[2]) }
             portfolio.append((info))
 
     return portfolio
  
-def read_prices(filename):
+def read_prices(filename2):
 
     prices = {}
-    with open(filename,'rt') as f:
+    with open(filename2) as f:
         rows = csv.reader(f)
         for row in rows:
             try:
@@ -30,26 +29,25 @@ def read_prices(filename):
                 pass
 
     return prices 
- 
-# def read_prices(filename):
-    # prices=[]
-    # with open(filename) as f:
-        # rows = csv.reader(f)
-        # for row in rows:
 
-			# prices.append((row))
 
-    # return prices
+filename1=sys.argv[1]
+name1 = read_portfolio(filename1,'rt')
 
-filename=sys.argv[1]
-name1 = read_portfolio(filename,'rt')
-
-# for i in range(len(name1)):
-    # print (name1[i])
 	
-filename=sys.argv[2]
-name2=read_prices(filename)
+filename2=sys.argv[2]
+name2=read_prices(filename2)
 
+
+
+def print_report(reportdata):
+    headers = ('Name','Shares','Price','Change')
+    print('%10s %10s %8s %8s' % headers)
+    print(('-'*10 + ' ')*len(headers))
+    for row in reportdata:
+        print(f"\t{row['name']}\t{row['shares']}\t  {row['price']}\t  {round(row['price']-name2[row['name']],2)}")
+
+print_report(name1)
 
 cost_portfolio=0.0
 for i in name1:
@@ -60,21 +58,6 @@ cost_price=0.0
 for i in name1:
     cost_price=cost_price+i['shares']*name2[i['name']]
 print(f"value currently: {cost_price}")
-
 print(f"current worth: {round(cost_price-cost_portfolio,2)}")
-# pricename=np.zeros(30)
-# currentprice=np.zeros(30)
-# for i in range(len(name2)):
-	# if name[i]==str('):
-		# print name[i]
-# for i in range(len(name2)):
-    # print (name2[i])
-#
-	
-headers = ('Name', 'Shares', 'Price', 'Change')
-print('%10s %10s %8s %8s' % headers)
-print(('-' * 10 + ' ') * len(headers))
-for row in name1:
-    print (f"\t{row['name']}\t{row['shares']}\t  {row['price']}\t  {round(row['price']-name2[row['name']],2)}")
 
 	
