ed113dad0305112a08bbc3833b6f4a44c6e81f3b
Brad McNiven <bm2570@mun.ca>
2021-05-29 13:23:36 -0230

    Completed exercise 3.7 - had to use parts of def in solution since I got stuck

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,24 +3,48 @@
 # Exercise 3.3
 
 import csv
+import sys
 
-def parse_csv(filename):
-    '''
-    Parse a CSV file into a list of records
-    '''
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+
     with open(filename) as f:
         rows = csv.reader(f)
 
-        # Read the file headers
-        headers = next(rows)
+
+        headers = next(rows) if has_headers else []
+
+
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+
         records = []
+		
         for row in rows:
-            if not row:    # Skip rows with no data
+		
+            if not row:   
                 continue
-            record = dict(zip(headers, row))
+
+            if select:
+                row = [row[index] for index in indices]
+				
+            if types:
+                row=[func(val) for func, val in zip(types,row)]
+	
+            if headers:
+                record=dict(zip(headers,row))
+			
+            else:
+                record=tuple(row)
+		            
+            #record = dict(zip(headers, row))
             records.append(record)
 
     return records
-	
-portfolio = parse_csv('Data/portfolio.csv')
+
+csvfile=sys.argv[1]
+portfolio = parse_csv(csvfile)
 print (portfolio)
+
